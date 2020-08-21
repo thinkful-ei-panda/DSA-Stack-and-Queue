a26fb1a5d156c2083ea099b4d086b66e7098b696
Matthew Rougely <matthewrougelysr@gmail.com>
2020-08-21 00:11:00 -0700

    3 complete

--- stacks.js
+++ stacks.js
@@ -37,22 +37,67 @@ class Stack {
   }
 }
 
-function main() {
-  let starTrek = new Stack();
-
-  starTrek.push("Kirk");
-  starTrek.push("Spock");
-  starTrek.push("McCoy");
-  starTrek.push("Scotty");
-
-  console.log(starTrek.top.data);
-  console.log(starTrek.top.next);
-  console.log(starTrek.top.next.data);
-  console.log(starTrek.top.next.next);
-  console.log(starTrek.top.next.next.data);
-  console.log(starTrek.top.next.next.next);
-  console.log(starTrek.top.next.next.next.data);
-  console.log(starTrek.top.next.next.next.next);
+function peek(item) {
+  console.log(item.top);
 }
 
-main();
+function isEmpty(item) {
+  if (item.top === null) {
+    console.log(`No data here bro`);
+  } else {
+    console.log(`Yeah my dude, there is some data here`);
+  }
+}
+
+function display(item) {
+  console.log(item.top.next);
+}
+
+let starTrek = new Stack();
+let blankTrek = new Stack();
+
+starTrek.push("Kirk");
+starTrek.push("Spock");
+starTrek.push("McCoy");
+starTrek.push("Scotty");
+
+console.log(`-----------startTrek Stack tests-----------------`);
+console.log(starTrek.top.data);
+console.log(starTrek.top.next);
+console.log(starTrek.top.next.data);
+console.log(starTrek.top.next.next);
+console.log(starTrek.top.next.next.data);
+console.log(starTrek.top.next.next.next);
+console.log(starTrek.top.next.next.next.data);
+console.log(starTrek.top.next.next.next.next);
+
+console.log(`-----------helper function tests-----------------`);
+peek(starTrek);
+isEmpty(starTrek);
+
+peek(blankTrek);
+isEmpty(blankTrek);
+
+display(starTrek);
+
+console.log(`-----------McCoy Pop-----------------`);
+starTrek.pop("McCoy");
+
+display(starTrek);
+
+function is_palindrome(s) {
+  s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  if (s === s.split("").reverse().join("")) {
+    console.log(`yes ${s} this is a mf palindrone`);
+  } else {
+    console.log(`Naw Bruh, ${s} ain't no palindrone`);
+  }
+}
+
+// True, true, true, false
+console.log(is_palindrome("dad"));
+console.log(is_palindrome("A man, a plan, a canal: Panama"));
+console.log(is_palindrome("1001"));
+console.log(is_palindrome("Tauhida"));
+console.log(is_palindrome("allow"));
+console.log(is_palindrome("lol"));
